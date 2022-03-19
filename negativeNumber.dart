c4579b517d836799cdd501a6bcec9b0e175852f8
Charles Paiva <64063918+charlespaiva@users.noreply.github.com>
2022-03-18 21:58:26 -0300

    Update negativeNumber.dart

--- negativeNumber.dart
+++ negativeNumber.dart
@@ -1,13 +1,13 @@
 import 'dart:math';
 
 int negativeGenerator(int x, int y) {
-  int a = Random().nextInt(x);//positive
+  int positiveMaximum = Random().nextInt(x);//positive
   print('Positive:$a');
-  int b = Random().nextInt(y);//negative
+  int negativeMinimum = Random().nextInt(y);//negative
   print('Negative $b');
   
-  b = b*-1;
+  negativeMinimum = negativeMinimum*-1;
   
-  int c = a + b;
-  return c;
+  int randomNumber = positiveMaximum + negativeMinimum;
+  return randomNumber;
 }
